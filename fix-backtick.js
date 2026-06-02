34121be4890e3893ca23b0c38b0627bcbc254cbc
Nguyen Quan Huy <quanghuystd8@gmai.com>
2026-06-03 05:54:47 +0700

    fix: restore backtick in addXp call

---
+++ fix-backtick.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('app/learn/toeic/theme/[id]/page.tsx', 'utf8');
+c = c.replace('addXp(15, \toeic-theme:);', 'addXp(15, 	oeic-theme:);');
+fs.writeFileSync('app/learn/toeic/theme/[id]/page.tsx', c, 'utf8');
+console.log('Done');
+console.log(c.slice(c.indexOf('addXp')-5, c.indexOf('addXp')+60));
