3d11fc1ec497f1f8f8fc34b738b25f8a4001866b
Nguyen Quan Huy <quanghuystd8@gmai.com>
2026-06-03 05:56:26 +0700

    fix: restore template literal in addXp

---
+++ fix-backtick2.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('app/learn/toeic/theme/[id]/page.tsx', 'utf8');
+const i = c.indexOf('addXp(15,');
+console.log('before:', JSON.stringify(c.slice(i, i+30)));
+c = c.slice(0, i) + 'addXp(15, 	oeic-theme:)' + c.slice(c.indexOf(');', i)+2);
+fs.writeFileSync('app/learn/toeic/theme/[id]/page.tsx', c, 'utf8');
+const v = fs.readFileSync('app/learn/toeic/theme/[id]/page.tsx', 'utf8');
+console.log('after:', v.slice(v.indexOf('addXp(15,'), v.indexOf('addXp(15,')+55));
