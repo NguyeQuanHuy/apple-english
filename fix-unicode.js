7944db344749173b97c3b5ed20985225d6420f5c
Nguyen Quan Huy <quanghuystd8@gmai.com>
2026-06-04 03:50:21 +0700

    fix: decode unicode escapes in beginner page

---
+++ fix-unicode.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('app/learn/beginner/page.tsx', 'utf8');
+// Decode all unicode escapes
+c = c.replace(/\\\\u([0-9a-fA-F]{4})/g, (m, code) => String.fromCharCode(parseInt(code, 16)));
+fs.writeFileSync('app/learn/beginner/page.tsx', c, 'utf8');
+console.log('Done. Sample:', fs.readFileSync('app/learn/beginner/page.tsx','utf8').slice(500,550));
