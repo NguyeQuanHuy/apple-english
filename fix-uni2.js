e13459b3a31e003ddee439027666088bcc3c37d1
Nguyen Quan Huy <quanghuystd8@gmai.com>
2026-06-04 03:51:07 +0700

    fix: decode unicode in beginner page

---
+++ fix-uni2.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('app/learn/beginner/page.tsx', 'utf8');
+c = c.replace(/\\u([0-9a-fA-F]{4})/g, (m, code) => String.fromCharCode(parseInt(code, 16)));
+fs.writeFileSync('app/learn/beginner/page.tsx', c, 'utf8');
+const v = fs.readFileSync('app/learn/beginner/page.tsx', 'utf8');
+const i = v.indexOf('nh');
+console.log('sample:', v.slice(i, i+30));
