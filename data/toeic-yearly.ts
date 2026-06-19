53d54762da1c37697d0c4b6692b66b02c90ceac8
Nguyen Quan Huy <quanghuystd8@gmai.com>
2026-06-20 05:33:11 +0700

    fix: add missing toeic-yearly data file

---
+++ data/toeic-yearly.ts
@@ -0,0 +1,15 @@
+﻿export type ToeicYearlySet = {
+  id: string;
+  year: number;
+  title: string;
+  questions: {
+    id: number;
+    part: number;
+    question: string;
+    options: string[];
+    answer: string;
+    explanation?: string;
+  }[];
+};
+
+export const TOEIC_YEARLY: ToeicYearlySet[] = [];
